f1115752b386275af6d1ef9601bb9a4f678cf0b4
Saurab Rajbanshi <saurab57322@gmail.com>
2025-01-19 21:26:47 +0545

    add initial files

---
+++ ToDoListApp/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoListApp.Models
+{
+	public class ToDoItem
+	{
+		public int ID { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
+		public bool IsCompleted { get; set; }
+	}
+}
