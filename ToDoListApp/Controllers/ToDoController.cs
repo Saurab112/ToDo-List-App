a049a7f8bbd4414afcb13dc3d0ef788041ea8e7f
Saurab Rajbanshi <saurab57322@gmail.com>
2025-01-20 10:15:02 +0545

    updated but edit not working

---
+++ ToDoListApp/Controllers/ToDoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Controllers
+{
+	public class ToDoController : Controller
+	{
+		public List<ToDoItem> items_list { get; set; } = new List<ToDoItem>()
+		{
+			new ToDoItem { Title = "coding", Description = "work on linq", ID = 1},
+			new ToDoItem { Title = "abc", Description = "aaa", ID = 2}
+		};
+
+		public IActionResult Index()
+		{
+			return View(items_list);
+		}
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			ToDoItem? item = items_list.FirstOrDefault(p=>p.ID == id);
+			if (item == null)
+			{
+				return NotFound();
+			}
+			return View(item);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(ToDoItem item)
+		{
+			Console.WriteLine($"Received ID: {item.ID}, Title: {item.Title}, Description: {item.Description}, IsCompleted: {item.IsCompleted}");
+			var existingItem = items_list.FirstOrDefault(temp=>temp.ID == item.ID);
+			if (existingItem != null)
+			{
+				existingItem.Title = item.Title;
+				existingItem.Description = item.Description;
+				existingItem.IsCompleted = item.IsCompleted;
+			}
+			
+			
+			return RedirectToAction("Index");
+		}
+		public IActionResult Delete()
+		{
+			return View();
+		}
+	}
+}
